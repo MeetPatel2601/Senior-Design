c32af23ccc2f4419a4c2ba66b261cc76007a09ab
MeetPatel2601 <133596670+MeetPatel2601@users.noreply.github.com>
2023-05-14 12:51:57 -0500

    Add files via upload

---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+
+ function Home() {
+    return (
+        <>
+            <Header />
+            <Main />
+            
+            {/* <Footer /> */}
+        </>
+    );
+ }
+
+
+export default Home;
